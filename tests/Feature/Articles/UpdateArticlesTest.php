1bb25a902daeb9bf15c2292ed6c8f21397367aa6
Faustin Vasquez Limon <vasquez.limon@gmail.com>
2021-01-26 09:59:47 -0800

    Policies to update an delete articles

--- tests/Feature/Articles/UpdateArticlesTest.php
+++ tests/Feature/Articles/UpdateArticlesTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Feature\Articles;
 
 use App\Models\Article;
+use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Laravel\Sanctum\Sanctum;
 use Tests\TestCase;
@@ -22,7 +23,7 @@ class UpdateArticlesTest extends TestCase
     }
 
     /** @test */
-    public function authenticated_users_can_update_articles()
+    public function authenticated_users_can_update_their_articles()
     {
        $article = Article::factory()->create();
 
@@ -40,6 +41,31 @@ class UpdateArticlesTest extends TestCase
        ->assertStatus(200);
 
        $this->assertDatabaseHas('articles',[
+           'title' => 'Title Changed',
+           'slug' => 'title-changed',
+           'content' => 'Content Changed',
+       ]);
+    }
+
+    /** @test */
+    public function authenticated_users_cannot_update_others_articles()
+    {
+       $article = Article::factory()->create();
+
+       Sanctum::actingAs($user = User::factory()->create());
+
+       $this->jsonApi()->withData([
+           'type' => 'articles',
+           'id' => $article->getRouteKey(),
+           'attributes' =>[
+               'title' => 'Title Changed',
+               'slug' => 'title-changed',
+               'content' => 'Content Changed',
+           ]
+       ])->patch(route('api.v1.articles.update',$article))
+       ->assertStatus(403);
+
+       $this->assertDatabaseMissing('articles',[
            'title' => 'Title Changed',
            'slug' => 'title-changed',
            'content' => 'Content Changed',
